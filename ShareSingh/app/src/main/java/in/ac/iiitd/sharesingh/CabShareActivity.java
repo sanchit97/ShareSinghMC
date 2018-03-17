95a8543f87b8c922d81d78b3e6cbedd5044b88e0
Sanchit  Sinha <sanchit@Sanchits-MacBook-Pro.local>
2018-03-17 13:51:08 +0530

    Activity edit proper
    Signed-off-by: Sanchit  Sinha <sanchit@Sanchits-MacBook-Pro.local>

--- ShareSingh/app/src/main/java/in/ac/iiitd/sharesingh/CabShareActivity.java
+++ ShareSingh/app/src/main/java/in/ac/iiitd/sharesingh/CabShareActivity.java
@@ -1,11 +1,14 @@
 package in.ac.iiitd.sharesingh;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class CabShareActivity extends AppCompatActivity {
 
@@ -15,5 +18,14 @@ public class CabShareActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cab_share);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+    public void goToCabShare(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Trip Created";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
